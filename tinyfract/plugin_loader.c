09a4fbacb05df18a0c7581561441544886cc4dc2
archur <archur>
2004-12-09 13:30:42 +0000

    Routine und Headerfile um Verzeichnisse nach Plugins zu durchsuchen.

---
+++ tinyfract/plugin_loader.c
@@ -0,0 +1,37 @@
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+void *load_plugins(char* plugin_path, char* output_method)
+{
+	void *handle;
+	output_initialize_view_port_t* fp;
+	
+	while plugin in plugin_path
+	{
+
+	handle=dlopen(plugin, RTLD_NOW); 
+	if(handle)
+	{
+		printf("%s\n",dlerror());
+		exit(EXIT_FAILURE);
+	}
+
+	fp=dlsym(handle,output_method);
+
+	if fp=!NULL break;
+	
+	}
+
+	if fp==NULL
+	{
+		printf("Error: output method %s does not exist.",output_method);
+	}
+
+	
+	fp(hdjksdfh,fgjhdfsjkg);
+	
+	return fp;
+}
+	
---
+++ tinyfract/plugin_loader.h
@@ -0,0 +1,6 @@
+#ifndef PLUGIN_LOADER_H
+#define PLUGIN_LOADER_H
+
+int load_library(char* plugin_name);
+
+#endif
