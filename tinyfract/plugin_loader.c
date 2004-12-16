a802a802b9dea35db5900479ec3f73b9111a6dd1
archur <archur>
2004-12-16 15:00:53 +0000

    Parsen eines Pfades eingebaut.

--- tinyfract/plugin_loader.c
+++ tinyfract/plugin_loader.c
@@ -1,7 +1,4 @@
 #include <dlfcn.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "common.h"
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
@@ -9,29 +6,31 @@
 #include <string.h>
 #include <sys/types.h>
 
+#include "common.h"
 
-int main(int argc, char *argv[])
+void *load_plugins(char* plugin_path, char* output_method)
 { 
 	struct dirent* dir_stream;
+	void*          dl_handle;
 	DIR*           plugin_dir;
-	char*          plugin_path;
-	char*          plugin_path_component;
+	char*          plugin_directory;
 	char*          plugin_path_help;
+	char*          plugin_path_scratch;
+	output_initialize_view_port_t* fp=NULL;
+	
 
-	if ((plugin_path=malloc(strlen(argv[1])+1))==NULL)
+	if ((plugin_path_scratch=malloc(strlen(plugin_path)+1))==NULL)
 	{
 		perror("plugin_loader, malloc");
 		exit(EXIT_FAILURE);
 	}
-	plugin_path_help=plugin_path;
-	strcpy(plugin_path,argv[1]);
+	plugin_path_help=plugin_path_scratch;
+	strcpy(plugin_path_scratch,plugin_path);
 
-	plugin_path_component=strtok(plugin_path_help,":");
+	plugin_directory=strtok(plugin_path_help,":");
 	do
 	{
-	
-
-		if ((plugin_dir=opendir(plugin_path_component))==NULL)
+		if ((plugin_dir=opendir(plugin_directory))==NULL)
 		{
 			switch (errno)
 			{
@@ -46,58 +45,32 @@ int main(int argc, char *argv[])
 					perror("plugin_loader, opendir");
 					exit(EXIT_FAILURE);
 			}
-		
 		}
 		else
 		{
 			do
-			{
-				dir_stream=readdir(plugin_dir);
-				if (dir_stream==NULL) break;
-				printf("%s\n",dir_stream->d_name);
+			{	
+				dl_handle=dlopen(plugin_directory, RTLD_NOW); 
+				if (dl_handle)
+				{
+					printf("%s\n",dlerror());
+					exit(EXIT_FAILURE);
+				}
+				else
+				{
+					fp=dlsym(dl_handle,output_method);
+					if (fp!=NULL) goto end;
+					dlclose(dl_handle);
+						
+				}
 			}
 			while (dir_stream);
 		}
-
-		}
-	while ((plugin_path_component=strtok(NULL,":"))!=NULL);
-	
-	free(plugin_path);
-	return 0;
 	}
-	
+	while ((plugin_directory=strtok(NULL,":"))!=NULL);
 
-
-void *load_plugins(char* plugin_path, char* output_method)
-{
-	void *handle;
-	output_initialize_view_port_t* fp;
-	
-	while plugin in plugin_path
-	{
-	
-	
-		
-	handle=dlopen(directory, RTLD_NOW); 
-	if(handle)
-	{
-		printf("%s\n",dlerror());
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		fp=dlsym(handle,output_method);
-		if fp=!NULL break;
-	}
-	
-	}
-
-	if fp==NULL
-	{
-		printf("Error: output method %s does not exist.",output_method);
-	}
-
-	
+end:
+	free(plugin_path_scratch);
 	return fp;
 }
-	
+
