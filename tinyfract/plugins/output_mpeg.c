154dfdcac4e1a40caf5a0b29df6f57b8088f1769
archur <archur>
2005-11-24 14:48:45 +0000

    Neue Datei für das Erstellen von Fraktal Filmen. Funktioniert allerdings noch nicht!

---
+++ tinyfract/plugins/output_mpeg.c
@@ -0,0 +1,300 @@
+#include <ffmpeg/avcodec.h>
+#include <ffmpeg/avformat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../common.h"
+#include "../plugin.h"
+
+#define MAX_COLORS 65536
+#define COLOR_CEILING 65536 
+
+
+typedef struct
+{
+	float y;
+	float u;
+	float v;
+} yuv_t;
+
+typedef struct
+{
+	AVCodecContext* context;
+	AVFrame*        picture;
+	FILE*           output_file;
+	yuv_t*          colors;
+} mpeg_t;	
+
+
+/* Color mapping functions. */
+float Rh(float H)
+{
+	if(H<1/3) return 1-(3*H);	
+	else if (H>2/3) return 3*H-2;
+	else return 0;
+}
+
+float Gh(float H)
+{
+	if(H<1/3) return (3*H);	
+	else if (H>2/3) return 0;
+	else return 2-(3*H);
+}
+
+float Bh(float H)
+{
+	if(H<1/3) return 0;	
+	else if (H>2/3) return 3-(3*H);
+	else return (3*H)-1;
+}
+
+
+float R(float H,float S,float Br)
+{
+	return Br+Rh(H)*S*(1-Br);
+}
+
+float G(float H,float S,float Br)
+{
+	return Br+Gh(H)*S*(1-Br);
+}
+
+float B(float H,float S,float Br)
+{
+	return Br+Bh(H)*S*(1-Br);
+}
+
+/* Constructor and destructor for mpeg output. */
+static mpeg_t* constructor_mpeg(const view_dimension_t dimension, char args[])
+{
+	mpeg_t* context;
+	AVCodec* codec
+	int    i;
+	int    outbuf_size;
+	int    size;
+	float  H;
+	float  S;
+	float  Br;
+
+	char   des[3];
+	int    mod[3];
+	float  thres[3];
+
+	uint8_t* outbuf;
+	uint8_t* picture_buf;
+
+	int   iteration_steps;
+	char* output_args;
+	char* name;
+	
+
+	/* Get memory for the output context. */
+	if (!(context=malloc(sizeof(mpeg_t)))) return NULL;
+
+	/* Parse the output args and the safing file name. */
+	output_args=strtok(args,"-");
+	name=strtok(NULL,"-");
+	#ifdef DEBUG
+	fprintf(stderr,"Output args: %s\n", output_args);
+	fprintf(stderr,"Safing file: %s\n", name);
+	#endif
+
+	
+	/* Check if parameters were given. */
+	if(output_args==NULL)
+	{
+		fprintf(stderr,"Please insert output parameters\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Check safing file was given. */
+	if(name==NULL)
+	{
+		fprintf(stderr,"Please insert a safing file for the image.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Scan the argument string. */
+	sscanf(output_args,"%c%d,%f%c%d,%f%c%d,%f",
+		&des[0],&mod[0],&thres[0],
+		&des[1],&mod[1],&thres[1],
+		&des[2],&mod[2],&thres[2]);
+	
+	
+	/* Just for help now. */
+	iteration_steps=mod[0]*mod[1]*mod[2];
+
+
+	/* Alloc memory for colors. */
+	context->colors=malloc(sizeof(yuv_t)*iteration_steps);
+
+	/* Allocate colors */
+	for(i=0;i<iteration_steps;i++)
+	{
+		int x=i;
+		int d;
+		
+		for (d=0;d<3;d++)
+		{
+			if (thres[d]<0 || thres[d]>=1)
+			{
+				fprintf(stderr,"Illegal output format %s.\n",output_args);
+				exit(EXIT_FAILURE);
+			}
+			switch (des[d])
+			{
+				case 'h':
+				case 'H':
+					H=(float)(x%mod[d])/mod[d]+thres[d];
+					if (H>1) H=H-1;
+					x=x/mod[d];
+					break;
+				case 's':
+				case 'S':
+					S=(float)(x%mod[d])/mod[d]+thres[d];
+					if (S>1) S=S-1;
+					x=x/mod[d];
+					break;
+				case 'b':
+				case 'B':
+					Br=(float)(x%mod[d])/mod[d]+thres[d];
+					if (Br>1) Br=Br-1;
+					x=x/mod[d];
+					break;
+					
+				default: 
+					fprintf(stderr,"Illegal output format %s.\n",args);
+					exit(EXIT_FAILURE);
+			}	
+		}
+		
+		context->colors[i].y=round(0.256788*R(H,S,Br)+0.504129*G(H,S,Br)+0.097906*B(H,S,Br))+16;
+		context->colors[i].u=round(-0.148223*R(H,S,Br)-0.290993*G(H,S,Br)+0.439216*B(H,S,Br))+128;
+		context->colors[i].v=round(0.439216*R(H,S,Br)-0.367788*G(H,S,Br)-0.071427*B(H,S,Br))+128;
+	}
+
+	/* Find the mpeg1 video encoder. */
+	codec=avcodec_find_encoder(CODEC_ID_MPEG1VIDEO);
+
+	/* Get the context. */
+	context->context=avcodec_alloc_context();
+
+	/* Get memory for the picture. */
+	context->picture=avcodec_alloc_frame();
+
+	/* put sample parameters */
+	context->context->bit_rate = 400000;
+	/* resolution must be a multiple of two */
+	context->context->width = 352;  
+	context->context->height = 288;
+	/* frames per second */
+	context->context->frame_rate = 25;  
+	context->context->frame_rate_base= 1;
+	/* emit one intra frame every ten frames */
+	context->context->gop_size = 10;
+	context->context->max_b_frames=1;
+
+	/* open it */
+	if (avcodec_open(context->context,codec) < 0)
+	{
+		fprintf(stderr, "could not open codec\n");
+		exit(1);
+	}
+
+	/* the codec gives us the frame size, in samples */
+
+	context->output_file=fopen(filename, "wb");
+	if (!f) {
+		fprintf(stderr, "could not open %s\n", filename);
+		exit(1);
+	}
+    
+	/* alloc image and output buffer */
+	outbuf_size=100000;
+	outbuf=malloc(outbuf_size);
+	size=context->context->width*context->context->height;
+	picture_buf=malloc((size * 3) / 2); /* size for YUV 420 */
+
+	context->picture->data[0]=picture_buf;
+	context->picture->data[1]=concontext->picture->data[0]+size;
+	context->picture->data[2]=context->picture->data[1] + size / 4;
+	context->picture->linesize[0]=context->context->width;
+	context->picture->linesize[1]=context->context->width / 2;
+	context->picture->linesize[2]=context->context->width / 2;
+
+	/* Return the handle. */
+	return context;
+}
+
+void destructor_mpeg(mpeg_t* handle)
+{
+	/* Close open image and safing file. */
+	fclose(handle->output_file);
+	gdImageDestroy(handle->im);
+	
+	free(handle);
+}
+
+/* Blit rectangle from pixelbuffer to X11 viewport. */
+void blit_rect_mpeg(mpeg_t* handle, const view_position_t position, const view_dimension_t dimension, pixel_value values[])
+{
+}
+
+
+/* Fill rectangle in image with color. */
+void fill_rect_mpeg(mpeg_t* handle, const view_position_t position, const view_dimension_t dimension, const pixel_value value)
+{
+	int x;
+	int y;
+
+	/* Put a filled rectangle in the image. */
+	/* Dupble for loop */
+	for(x=position.x;x<=dimension.width+position.x;x++)
+	{
+		for(y=position.y;y<=dimension.height+position.y;y++)
+			gdImageSetPixel(handle->im,x,y,handle->colors[value]);
+	}
+}
+
+/* Put pixel into the image viewport. */
+void put_pixel_mpeg(mpeg_t* handle, const view_position_t position, const pixel_value value)
+{
+	/* Put a pixel into the image. */
+	gdImageSetPixel(handle->im,position.x,position.y,handle->colors[value]);
+}
+
+/* Safe the image */
+void flush_viewport_mpeg(mpeg_t* handle, button_event_t* position)
+{
+	/* Safe the image. */
+	gdImagePng(handle->im,handle->output_file);
+	
+	/* Call the main function that the programm is over. */
+	position->type=autozoom_quit;
+
+	return;
+}
+
+
+/* Enumerate plugin facilities. */
+volatile const plugin_facility_t tinyfract_plugin_facilities[]=
+{
+	{
+		name: "mpeg",
+		type: plugin_facility_output,
+		facility:
+		{
+			output:
+			{
+				constructor:             (const plugin_output_constructor_t*) &constructor_mpeg,
+				destructor:              (const plugin_output_destructor_t*) &destructor_mpeg,
+				blit_rect_function:      (const plugin_output_blit_rect_function_t*) &blit_rect_mpeg,
+				fill_rect_function:      (const plugin_output_fill_rect_function_t*) &fill_rect_mpeg,
+				flush_viewport_function: (const plugin_output_flush_viewport_function_t*) &flush_viewport_mpeg,
+				put_pixel_function:      (const plugin_output_put_pixel_function_t*) &put_pixel_mpeg,
+			}
+		}
+	},
+	{ plugin_facility_end }
+};
+
